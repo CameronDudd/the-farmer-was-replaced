44f75f9a37a647798a1ec69d14f58069b29cc47a
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2025-10-16 21:34:22 +0100

    feat(utils): add utility functions and soil lookup for entities

--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
+EntityNeedsSoilLookup = {
+    Entities.Grass: False,
+    Entities.Bush: False,
+    Entities.Tree: False,
+    Entities.Carrot: True,
+    Entities.Pumpkin: True
+}
+
 def restart():
     clear()
     while not can_harvest():
         pass
+
+def xor(a, b):
+    return (a or b) and not (a and b)
+
+def ternary(condition, if_true, if_false):
+    if condition:
+        return if_true
+    return if_false
 
 def submov(current_pos, target_pos, direction, inv_direction, max_pos):
     direction_diff = 0
@@ -24,3 +40,18 @@ def mov(x, y):
     n = get_world_size()
     submov(get_pos_x(), x, East, West, n)
     submov(get_pos_y(), y, North, South, n)
+
+
+def do_simple_patch(x0, y0, x1, y1, entity):
+    mov(x0, y0)
+    for y in range(y0, y1 + 1):
+        for x in range(x0, x1 + 1):
+            if can_harvest():
+                harvest()
+            if not (EntityNeedsSoilLookup[entity] and (get_ground_type() == Grounds.Soil)):
+                till()
+            plant(entity)
+            if (x < x1):
+                move(East)
+        if (y < y1):
+            mov(x0, y + 1)
